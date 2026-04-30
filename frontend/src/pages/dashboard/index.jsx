29a0e2eb704b0a7ed090e9ea459904e7fe63a2f9
NickolasHardRock <nickolaslamin@gmail.com>
2026-04-29 22:13:14 -0300

    feat: Add styling for Dashboard component

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,10 +1,11 @@
 import e from "cors";
 import { useState } from "react";
 import MainLayout from "../../layouts/MainLayout";
+import "./style.css"
 function Dashboard(){
     return(
         <>
-            <h1>Dashboard</h1>
+            <h1 className="Dashboard">Dashboard</h1>
             <MainLayout></MainLayout>
         </>
     )
