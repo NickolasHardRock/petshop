55e85aa6d86e838409e6c1c21e7eb62dca7fbe70
NickolasHardRock <nickolaslamin@gmail.com>
2026-06-03 19:35:59 -0300

    feat: Integrate MainLayout into AppRoutes for improved layout structure

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,15 +4,19 @@ import Dashboard from '../pages/dashboard'
 import PrivateRoute from "./PrivateRoute";
 import Pets from "../pages/pets";
 import Owners from "../pages/owners";
+import MainLayout from "../layouts/MainLayout";
 
 function AppRoutes() {
     return (
+        <div>
+        <MainLayout/>
         <Routes>
-            <Route path='/' element={<Login></Login>} />
+                <Route path='/' element={<Login></Login>} />    
                 <Route path='/dashboard' element={<Dashboard />} />
                 <Route path='/pets' element={<Pets />} />
                 <Route path='/owners' element={<Owners />} />
         </Routes>
+        </div>
     )
 }
 
